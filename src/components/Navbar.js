b653d7198371bd5f9e96504a29c3a68bea05170d
David Singleton <davidsingleton@gmail.com>
2021-02-08 18:43:59 -0800

    title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ function Navbar(props) {
       <AppBar position="static" color="transparent" elevation={0}>
         <Container disableGutters={true}>
           <Toolbar>
-            <Typography>Spreadsheet</Typography>
+            <Typography variant="h4">🧮 Spreadsheet</Typography>
             <div className={classes.spacer} />
               <IconButton
                 color="inherit"
